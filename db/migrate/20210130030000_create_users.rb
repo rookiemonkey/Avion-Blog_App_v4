8bf23f74686f130bae51ec8fa91f15a27ffb5c2c
rookiemonkey <kevinroirigorbasina@pm.me>
2021-01-30 11:42:27 +0800

    add user model

--- db/migrate/20210130030000_create_users.rb
+++ db/migrate/20210130030000_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string, :first_name
-      t.string, :last_name
-      t.username, :username
-      t.email, :email
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_014043) do
+ActiveRecord::Schema.define(version: 2021_01_30_030000) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
