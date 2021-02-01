4a0972ad469e93e87ea8502f946126972ec02718
rookiemonkey <kevinroirigorbasina@pm.me>
2021-02-01 23:41:28 +0800

    able now to create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show]
+
     def index
     end
 
@@ -10,6 +12,29 @@ class ArticlesController < ApplicationController
     end
 
     def show
+    end
+
+    def create
+        begin
+            @article = Article.new(self.extract_article_params)
+            raise 'Failed to save' unless @article.save
+            redirect_to @article
+
+        rescue Exception => e
+            render :new
+
+        end
+    end
+
+
+    private
+
+    def set_article
+        @article = Article.find params[:id]
+    end
+
+    def extract_article_params
+        params.require(:article).permit(:title, :body)
     end
 
 end
