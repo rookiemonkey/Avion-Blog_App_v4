6e1183daa2fe466393b912ca166c218cf8068f33
rookiemonkey <kevinroirigorbasina@pm.me>
2021-02-03 07:53:53 +0800

    able now to purge images on disk storage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,6 +29,7 @@ class ArticlesController < ApplicationController
 
     def update
         begin
+            @article.image.purge if self.is_new_image?
             raise 'Failed to update article' unless @article.update(self.extract_article_params)
             redirect_to @article
             
@@ -40,6 +41,7 @@ class ArticlesController < ApplicationController
 
     def destroy
         begin
+            @article.purge
             raise 'Failed to delete article' unless @article.destroy
             redirect_to articles_path
 
@@ -58,6 +60,10 @@ class ArticlesController < ApplicationController
 
     def extract_article_params
         params.require(:article).permit(:title, :body, :image)
+    end
+
+    def is_new_image?
+        !(!!params.require(:article).permit(:image))
     end
 
 end
