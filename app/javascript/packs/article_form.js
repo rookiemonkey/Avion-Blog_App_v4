a3c3a669058fa0d7eb62b9bf331ba63afac8ea11
rookiemonkey <kevinroirigorbasina@pm.me>
2021-02-03 08:46:15 +0800

    able now to change the image to preview the image about to be uploaded

--- app/javascript/packs/article_form.js
+++ app/javascript/packs/article_form.js
@@ -1,9 +1,17 @@
-const input = document.querySelector('input[type="file"]')
-const label = document.querySelector('span.file-label')
+const input = document.querySelector('input[type="file"]');
+const label = document.querySelector('span.file-label');
+const preview = document.querySelector('img.preview');
 
-input.onchange = () => label.textContent = parseFileName()
+input.onchange = () => {
 
-function parseFileName() {
+  // parse file name
   const last_slash = input.value.lastIndexOf(`\\`)
-  return input.value.slice(last_slash + 1, input.value.length)
+  const fileName = input.value.slice(last_slash + 1, input.value.length)
+  label.textContent = fileName
+
+  // preview uploaded image 
+  const reader = new FileReader();
+  reader.readAsDataURL(input.files[0])
+  reader.onload = e => preview.setAttribute('src', e.target.result)
+
 }
