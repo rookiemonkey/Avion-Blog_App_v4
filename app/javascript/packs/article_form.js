9774e3d9a3bbc8a9289188fe5d089abb585d317a
rookiemonkey <kevinroirigorbasina@pm.me>
2021-02-03 09:02:25 +0800

    able now to validate image on front-end

--- app/javascript/packs/article_form.js
+++ app/javascript/packs/article_form.js
@@ -3,6 +3,15 @@ const label = document.querySelector('span.file-label');
 const preview = document.querySelector('img.preview');
 
 input.onchange = () => {
+  const file = input.files[0];
+
+  // check file size
+  if (file.size > 1000000) return alert('File is too big')
+
+  // check mime type
+  const valid_mime = ['image/png', 'image/jpeg', 'image/jpg']
+  const is_valid = valid_mime.some(t => t == file.type)
+  if (!is_valid) return alert('We only accept jpeg, jpg and png files')
 
   // parse file name
   const last_slash = input.value.lastIndexOf(`\\`)
